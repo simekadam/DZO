e89e061beeefe9978a306be159e4ab9947efaed1
simekadam <simekadam@gmail.com>
2012-10-09 11:35:44 +0200

    2nd also with image

--- hw2/filtering_times.m
+++ hw2/filtering_times.m
@@ -10,20 +10,29 @@ function [t_2D, t_separable] = filtering_times(im, sigmas)
 % a 2D Gaussian, and using the separable kernels. The timings are stored in
 % t_2D and t_separable, respectively.
 % Thus t_2D and t_separable are 1-by-K vectors.
-im = im2double(im);
+%im = im2double(im);
 
 for sigma = sigmas
 	% find closest greater odd integer ≤ 3·sigma
 	filter_size = (ceil((ceil(sigma*3)/2)+0.5)*2)-1;
 	gauss_filter = fspecial('gauss', filter_size, sigma);
 	gauss_filter1D = fspecial('gauss', [filter_size, 1], sigma);
+	tic
 	filtered_image = conv2(im, gauss_filter, 'same');
+	toc
+	timer2d = toc;
+	tic
 	filtered_image2 = conv2(gauss_filter1D, gauss_filter1D, im, 'same');
+	toc
+	timer1d = toc;
+
+	fprintf('%i test %i\n', timer1d, timer2d);
 
 	% THIS CODE CHECKS IF ARE BOTH TRANSFORMS PRODUCING SAME RESULTS (SLIGHT DIFFERENCE IS CAUSED BY NUMERIC ROUNDING WHILE COMPUTING)
 	% im_diff = filtered_image - filtered_image2;
 
 	% [row_val row_ind] = max(abs(im_diff), [], 1);
 	% disp(max(row_val)<10e-13);
+
 
 end
